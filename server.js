22da7ef147eb2a154a603da39ca3a236493f6f55
Zach Grafton <zachgrafton@gmail.com>
2020-10-13 13:40:49 -0700

    added server functionality

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("hi");
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use('/src', express.static('src'));
+
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname + '/src/index.html')));
+
+app.listen(port, () => console.log(`port ${port}`));
